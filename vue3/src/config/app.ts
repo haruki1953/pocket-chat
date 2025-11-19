77840ab524ffcd49e6df3484090cb6d96ffcca00
haruki <kinikun@outlook.com>
2025-11-19 18:38:51 +0800

    fix: 修复音频文件重命名而导致的导入错误

--- vue3/src/config/app.ts
+++ vue3/src/config/app.ts
@@ -3,9 +3,9 @@ import appUserDefaultAvatar from '@/assets/logo.jpg'
 import appIcon from '@/assets/icon.png'
 import appIconNotif from '@/assets/icon-notif.png'
 
-import appMessageNotifSound from '@/assets/pop-up-off.mp3'
-import appMessageUpdateSound from '@/assets/pop-up-on.mp3'
-import appMessageSendSound from '@/assets/pop-down.mp3'
+import appMessageNotifSound from '@/assets/message-notif-sound.mp3'
+import appMessageUpdateSound from '@/assets/message-update-sound.mp3'
+import appMessageSendSound from '@/assets/message-send-sound.mp3'
 
 // 应用logo，用户默认头像
 export {
